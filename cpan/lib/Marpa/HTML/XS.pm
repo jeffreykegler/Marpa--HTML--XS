04a68d657873c018f8878bae851d80db4fcb7e28
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2012-01-02 21:53:24 -0800

    Building module.

--- cpan/lib/Marpa/HTML/XS.pm
+++ cpan/lib/Marpa/HTML/XS.pm
@@ -29,3 +29,42 @@ Carp::croak("Marpa::HTML did not load Marpa::XS")
     if $Marpa::HTML::MARPA_MODULE ne 'Marpa::XS';
 
 1;
+
+__END__
+
+=head1 NAME
+
+Marpa::HTML::XS -- XS-only version of Marpa::HTML
+
+=head1 SYNOPSIS
+
+C<use Marpa::HTML::XS;>
+
+=head1 DESCRIPTION
+
+This module uses L<Marpa::HTML> and L<Marpa::XS>.
+The documentation is with those two modules.
+
+L<Marpa::HTML> by itself own chooses between L<Marpa::XS> and L<Marpa::PP>.
+L<Marpa::HTML> runs L<Marpa::XS> if possible, L<Marpa::PP> otherwise.
+This is what users find most convenient, but there is a downside:
+since L<Marpa::XS> requires a number of non-Perl dependencies,
+the cpantesters almost always run with L<Marpa::PP>.
+This makes it difficult for users to determine if
+an XS-powered L<Marpa::HTML> has been tested on their system.
+The lop-sided testing of L<Marpa::HTML>
+is also a major quality assurance for both
+L<Marpa::HTML> and L<Marpa::XS>.
+This module redresses the balance.
+
+=head1 AUTHOR
+
+Jeffrey Kegler
+
+=head1 COPYRIGHT AND LICENSE
+
+  This software is copyright (c) 2011 by Jeffrey Kegler
+  This is free software; you can redistribute it and/or modify it
+  under the same terms as the Perl 5 programming language system
+  itself.
+
